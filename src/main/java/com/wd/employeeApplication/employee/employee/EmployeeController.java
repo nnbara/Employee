37ec22aa6ee971bff5be3cfff28bad676511335c
Neha Nutan Bara (Digital) <NE20090804@wipro.com>
2020-01-21 16:57:38 +0530

    employee modified

--- src/main/java/com/wd/employeeApplication/employee/employee/EmployeeController.java
+++ src/main/java/com/wd/employeeApplication/employee/employee/EmployeeController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.websocket.server.PathParam;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +27,6 @@ public class EmployeeController {
     @PostMapping
     public ResponseEntity registerEmployee(@RequestBody(required = true) Employee employee){
         employeeService.registerEmployee(employee);
-        /*System.out.println("Employee first name : "+ employee.getFirstName() );*/
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
@@ -32,20 +34,6 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAllEmployee(){
 
         List<Employee> employeeList=employeeService.getAllEmployee();
-
-        /*List<Employee> employeeList=new ArrayList<>();
-        Employee employee=new Employee();
-
-        employee.setEmployeeId(909);
-        employee.setFirstName("Raj");
-        employee.setLastName("Dev");
-        employee.setDesignation("Project Lead");
-        employee.setEmailId("dev@gmail.com");
-        employee.setPhoneNumber("9875261789");
-
-        employeeList.add(employee);
-        employeeList.add(employee);*/
-
         return new ResponseEntity<>(employeeList,HttpStatus.OK);
     }
 
@@ -53,28 +41,24 @@ public class EmployeeController {
     public ResponseEntity<Employee> getEmployee(@PathVariable (value = "employeeId") Integer employeeId){
 
         Employee employee=employeeService.getEmployee(employeeId);
-
-       /* employee.setEmployeeId(909);
-        employee.setFirstName("Raj");
-        employee.setLastName("Dev");
-        employee.setDesignation("Project Lead");
-        employee.setEmailId("dev@gmail.com");
-        employee.setPhoneNumber("9875261789");*/
-
         return new ResponseEntity<>(employee,HttpStatus.OK);
     }
 
     @DeleteMapping("{employeeId}")
     public ResponseEntity deleteEmployee(@PathVariable(value = "employeeId") int employeeId){
         employeeService.deleteEmployee(employeeId);
-        /*System.out.println("Deleted Employee id :"+employeeId);*/
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("{employeeId}")
     public ResponseEntity updateEmployee(@PathVariable(value = "employeeId")int employeeId, @RequestBody(required = true) Employee employee){
         employeeService.updateEmployee(employeeId,employee);
-        /*System.out.println("Employee is updated...");*/
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("uploadfile")
+    public ResponseEntity uploadFile(@PathParam("file")MultipartFile file)throws IOException {
+        employeeService.readFile(file);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/wd/employeeApplication/employee/employee/EmployeeService.java
+++ src/main/java/com/wd/employeeApplication/employee/employee/EmployeeService.java
@@ -1,8 +1,17 @@
 package com.wd.employeeApplication.employee.employee;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,5 +39,55 @@ public class EmployeeService {
     public void updateEmployee(int employeeId,Employee employee){
             employee.setEmployeeId(employeeId);
             employeeRepository.save(employee);
+    }
+    public void readFile(MultipartFile multipartFile)throws IOException {
+        File file=new File("file");
+        FileOutputStream fos=new FileOutputStream(file);
+        fos.write(multipartFile.getBytes());
+        fos.close();
+
+        FileInputStream excelFile=new FileInputStream(file);
+        Workbook workbook=new XSSFWorkbook(excelFile);
+        Sheet dataTypeSheet=workbook.getSheetAt(0);
+
+        Iterator<Row> iterator=dataTypeSheet.iterator();
+
+        List<Employee> employeeList=new ArrayList<>();
+
+        while (iterator.hasNext()){
+
+            Employee employee=new Employee();
+            DataFormatter dataFormatter=new DataFormatter();
+            Row row=iterator.next();
+
+            Iterator<Cell> cellIterator=row.cellIterator();
+
+            if(cellIterator.hasNext()){
+                Cell cell=cellIterator.next();
+                employee.setFirstName(dataFormatter.formatCellValue(cell));
+            }
+            if(cellIterator.hasNext()){
+                Cell cell=cellIterator.next();
+                employee.setLastName(dataFormatter.formatCellValue(cell));
+            }
+            if(cellIterator.hasNext()){
+                Cell cell=cellIterator.next();
+                employee.setDesignation(dataFormatter.formatCellValue(cell));
+            }
+            if(cellIterator.hasNext()){
+                Cell cell=cellIterator.next();
+                employee.setEmailId(dataFormatter.formatCellValue(cell));
+            }
+            if(cellIterator.hasNext()){
+                Cell cell=cellIterator.next();
+                employee.setPhoneNumber(dataFormatter.formatCellValue(cell));
+            }
+
+            employeeList.add(employee);
+        }
+        for(Employee emp: employeeList){
+            employeeRepository.save(emp);
+        }
+
     }
 }
